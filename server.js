615b61af23d98d191ffd311e407a7bba2840a175
Brad Goldsmith <b-rad@Brads-MBP.fios-router.home>
2017-06-12 12:07:24 -0400

    index.html and bundle.js file added to the public folder in our application

---
+++ server.js
@@ -0,0 +1,37 @@
+//requiring packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//ES6 version of setting up express app and port for application to be used
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//Creates a static public directory to be used
+app.use(express.static('./server'));
+
+//Setting up our mongoDB connection using ES6 format
+const link = 'mongodb://localhost/nytreact';
+
+mongoose.connect(link);
+const db = mongoose.connection;
+
+db.on('error', err => {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', () => {
+  console.log('Mongoose Connection Successful.');
+});
+
+
+
+//Sets up and listens on our defined port using ES6
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
