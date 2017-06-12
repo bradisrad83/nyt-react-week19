ebbd4f3347bcf426b7bceedbc0593a62fe405fb8
Brad Goldsmith <b-rad@Brads-MacBook-Pro.local>
2017-06-12 16:47:41 -0400

    started the react-routes and saved my API key for the NY Times

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,4 +1,6 @@
+//This is where all the NYT API information will be stored from
+//Exported so that we can use it from the user entered data
 var axios = require('axios');
 
 //Variable to store our APIKey
-const key
+const key = 'a55ac7da34a74cd2bff6d208c01c37e4';
---
+++ server/routes/react-routes.js
@@ -0,0 +1,17 @@
+//Include react library/router module
+var React = require('react');
+var router = require('react-router');
+
+// Include the Route component for displaying individual routes
+const Route = router.Route;
+
+// Include the Router component to contain all our Routes
+// Here where we can pass in some configuration as props
+const Router = router.Router;
+
+// Include the hashHistory prop to handle routing client side without a server
+// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
+const hashHistroy = router.hashHistroy;
+
+//Include the IndexRoute (catch-all route)
+const IndexRoute = router.IndexRoute;
